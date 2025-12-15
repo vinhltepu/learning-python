66fe1704e402c3a158f37b5cdee6fe12d5480bd7
vinhltepu <luuthanhvinh2811@gmail.com>
2025-12-15 21:54:51 +0700

    Bài 1
    Hoàn thành bài 1

---
+++ Bài 1/main.py
@@ -0,0 +1,11 @@
+### code python
+"""
+Bài 1: Toán tử số học
+Viết chương trình nhập vào 2 số nguyên a và b.
+Hãy in ra lần lượt:
+Tổng của a và b
+Hiệu của a và b
+Tích của a và b
+Thương chia lấy phần nguyên của a cho b
+Phần dư của a chia cho b
+"""
