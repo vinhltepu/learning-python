5f7e956511de684d0696b6ea993e7c3d357f7f44
vinhltepu <luuthanhvinh2811@gmail.com>
2025-12-24 20:34:51 +0700

    Update main.py

---
+++ Bài 2/main.py
@@ -0,0 +1,43 @@
+#Bài 1
+#Yêu cầu
+#In ra dòng chữ Hello World
+
+print('Hello World')
+
+
+
+
+#Bài 2
+#Yêu cầu
+#Tạo biến a = 10 và in ra giá trị của a
+  
+a = 10
+print(a)
+
+
+#Bài 3
+#Yêu cầu
+#Nhập tên từ bàn phím và in ra lời chào
+
+name = input('Nhap ten: ')
+print('Hello', name)
+
+
+#Bài 4
+#yêu cầu
+#Tạo hai biến x = 5, y = 3 và in ra tổng
+
+
+x = 5
+y = 3
+print(x + y)
+
+
+#Bài 5: In chuỗi nhiều dòng
+#Yêu cầu
+#Viết chương trình Python để in ra một đoạn văn bản gồm nhiều dòng mô tả việc học Python.
+
+
+print('dòng số 1 ')
+print('dòng số 2')
+print('dòng số 3')
