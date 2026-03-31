0318f0427a67fa1b1444fa93e328ec4430a1f564
vinhltepu <luuthanhvinh2811@gmail.com>
2026-03-31 22:01:39 +0700

    COOKIE, và Flash Messages

--- b . flask python/bài 6/main.py
+++ b . flask python/bài 6/main.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template , request , Response
+from click import clear
+from flask import Flask, flash, make_response, redirect, render_template , request , session, url_for
+from datetime import timedelta
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'ABC123'
 
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)  # thiết lập thời gian sống của session là 5 phút
+
 @app.route('/')
 def index():
+    flash("xin chao mọi người")
     return render_template('index.html')
+
+@app.route('/get-flash')
+def get_flash():
+    return get_flashed_messages()  # lấy tất cả các thông điệp flash đã được lưu trữ trong session và trả về chúng dưới dạng một danh sách. Các thông điệp này có thể được hiển thị cho người dùng trong giao diện của ứng dụng.
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,9 +26,66 @@ def login():
         password = request.form.get('password')
         
         if username == 'admin' and password == 'admin123':
+
+            session['account'] = username  # tạo session để lưu thông tin đăng nhập của người dùng 
+            session['is_logged_in'] = True  # đánh dấu người dùng đã đăng nhập thành công
+
+            session.permanent = True  # thiết lập session là vĩnh viễn (dựa trên PERMANENT_SESSION_LIFETIME)
             return render_template('manager-page.html')  # trả về trang quản lý nếu đăng nhập thành công    
+        
+        # cần tạo session nó vẫn nằm trong cái header của response nên nó sẽ được gửi về client và lưu trữ ở đó, khi client gửi request tiếp theo thì session sẽ được gửi kèm theo trong header của request đó, server sẽ kiểm tra session để xác định xem người dùng đã đăng nhập hay chưa và có quyền truy cập vào tài nguyên hay không.
         else:
             return 'Login failed'
 
+
+
+
+
+@app.route('/delete-session')
+def delete_session():
+    session.clear()  # xóa session để đăng xuất người dùng khỏi hệ thống
+    return redirect(url_for('login'))  # chuyển hướng về trang đăng nhập sau khi xóa session
+
+
+@ app.route('/create-cookie')
+def create_cookie():
+    response = make_response('Cookie đã được tạo')
+    response.set_cookie('demo', 'python',max_age=timedelta(minutes=5))  # tạo cookie với tên 'demo' và giá trị 'python'
+    return response
+
+@app.route('/get-cookie')
+def get_cookie():  
+    demo_cookie = request.cookies.get('demo')  # lấy giá trị của cookie 'demo' từ request
+    return f'Giá trị của cookie "demo" là: {demo_cookie}'
+
+@app.route('/delete-cookie')
+def delete_cookie():    
+    response = make_response('Cookie đã được xóa')
+    response.set_cookie('demo', '', expires=0)  # xóa cookie bằng cách đặt giá trị rỗng và thời gian hết hạn là 0
+    return response
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3333)
+
+## client (Lúc đầu chưa có session)
+# 1. Client gửi request tới server => Server response Client trang đăng nhập
+
+# 2. Client gửi request tới server và kèm theo dữ liệu trong FORM => SERVER kiểm tra dữ liệu trong FORM
+# Dữ liệu Trong Form được Server kiểm tra => chính xác bên server phản hồi về
+
+# Header:
+# HTTP/1.1 200 OK
+# Set-Cookie: session=fjsfksdjhfsjkdhfksdfsjkf.SDKJFGSDFLJG, PATH=/; HTTPOnly
+            # <account:tamdz>
+
+# 3. Client gửi resquest tới server:
+
+# REQUEST
+# header:
+# GET /login HTTP/1.1
+# cookie: session=fjsfksdjhfsjkdhfksdfsjkf.SDKJFGSDFLJG
--- b . flask python/bài 6/readme.md
+++ b . flask python/bài 6/readme.md
@@ -1,6 +1,24 @@
-## khái niệm về sessoion 
+# khái niệm về sessoion 
 
 Được lưu trên server (thường có Session ID gửi qua Cookie).
 Dùng để lưu thông tin tạm thời trong phiên làm việc của user, như đăng nhập.
 Hết thời gian hoặc đóng trình duyệt thì session thường bị xóa.
 Bảo mật hơn cookie vì không lưu thông tin trực tiếp trên client
+
+# - Cookie:
++ Được lưu trên trình duyệt của người dùng.
++ Dùng để lưu thông tin như token đăng nhập, tùy chọn giao diện, giỏ hàng...
++ Có thể được gửi lên server mỗi lần request.
++ Có thời gian sống do server hoặc client đặt ra.
+
+
+
+
+
+1. Client gửi request lên server => flash("Xin chào tôi đang học web development") => set-cookie
+server response về client
+Cái session đấy tồn tại cho tới khi nào mà bên phía server sử dụng cái function get_flashed_messages()
+
+2. client gửi request tới server thì bên phía server nhận dc request => cookie (session)
+server sử dụng function get_flashed_messages() => thì nó sẽ lấy hết các nội dung trong phần cookie (session mà client gửi tới)
+Thì server sẽ xử lí => server tạo response và trong cái response đấy server sẽ xóa cookie (session) đi rồi phản hồi về clien
