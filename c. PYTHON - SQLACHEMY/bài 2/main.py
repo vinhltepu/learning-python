c11e703466b5b6f8beb950c505d4d46659d9b428
vinhltepu <luuthanhvinh2811@gmail.com>
2026-04-02 19:27:24 +0700

    Các Thao Tác CRUD (CREATE, READ, UPDATE, DELETE)

---
+++ c. PYTHON - SQLACHEMY/bài 2/main.py
@@ -0,0 +1,24 @@
+from models import engine , Product
+from sqlalchemy.orm import Session
+
+# Engine dùng để kết nối tới cơ sở dữ liệu
+# Base dùng để tạo cơ sở dữ liệu và bảng.
+# Session dùng để quản lý và thao tác dữ liệu trong các bảng đó. 
+
+session = Session(bind=engine)
+# Đây là phầm thêm dữ liệu cho bảng products
+new_product_1 = Product(name="Laptop", price=1500.0) # Tạo một sản phẩm mới với tên "Laptop" và giá 1500.0
+new_product_2 = Product(name="Smartphone", price=800.0) # Tạo một sản phẩm mới với tên "Smartphone" và giá 800.0
+session.add_all([new_product_1, new_product_2])            # Thêm các sản phẩm mới vào session. Phương thức add_all() được sử dụng để thêm nhiều đối tượng cùng một lúc vào session. Ở đây, chúng ta thêm cả new_product_1 và new_product_2 vào session để chuẩn bị cho việc lưu chúng vào cơ sở dữ liệu. 
+session.commit()# Lưu các thay đổi vào cơ sở dữ liệu. Khi gọi commit(), tất cả các thay đổi đã được thực hiện trong session sẽ được ghi vào cơ sở dữ liệu, bao gồm việc thêm sản phẩm mới mà chúng ta vừa tạo.
+
+#đây là phần lấy dữ liệu ra từ bảng products - read 
+products = session.query(Product).all() # Truy vấn tất cả các sản phẩm từ bảng products. Phương thức query() được sử dụng để tạo một truy vấn mới, và all() được gọi để lấy tất cả các kết quả của truy vấn đó. Kết quả sẽ là một danh sách các đối tượng Product đại diện cho tất cả các sản phẩm trong bảng products.
+for product in products: # Duyệt qua từng sản phẩm trong danh sách products và in ra tên và giá của mỗi sản phẩm. 
+    print(f"Product Name: {product.name}, Price: {product.price}") # In ra tên và giá của mỗi sản phẩm. 
+
+
+# update dữ liệu 
+products[0].price = 1400.0 # Cập nhật giá của sản phẩm đầu tiên trong danh sách 
+products[1].name = "Smartphone Pro" # Cập nhật tên của sản phẩm thứ hai trong danh sách
+session.commit() 
---
+++ c. PYTHON - SQLACHEMY/bài 2/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base
+
+"<dialect>+<driver>://<username>:<password>@<host>:<port>/<database_name>"
+
+url = "sqlite:///learn_sqlite.db"  # .db .sqlite .sqlite3
+
+"pymysql"
+url_mysql = "mysql+pymysql://root:@127.0.0.1:3306/employee_java"
+
+engine = create_engine(url)
+
+Base = declarative_base()  # class
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(name="id", type_=Integer, primary_key=True)
+    name = Column(name="product_name", type_=String(50))
+    price = Column(name="product_price", type_=Float)
+
+Base.metadata.create_all(engine)
