7563eb25d0535d0ccdca348c902a743455a3f3b2
vinhltepu <luuthanhvinh2811@gmail.com>
2026-07-07 21:39:00 +0700

    tạo trang chủ cho API

--- grocery-store-fastAPI/main.py
+++ grocery-store-fastAPI/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi import Request
 from app.core.config import settings
 from app.db.session import engine
 from app.db import base  #kích hoạt import models để create_all biết bảng nào cần tạo
-
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from app.api.endpoints import products, customers, invoices, stats
 
 # Tạo tất cả các bảng trong cơ sở dữ liệu nếu chưa tồn tại
@@ -13,13 +15,20 @@ app = FastAPI(
     description="API quản lý cửa hàng tạp hóa ngành mộc Quang Thúy",  
     version="1.0.0",                                                     
 )
+# khai báo thư mục static và templates để FastAPI có thể phục vụ các file tĩnh và template HTML
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(products.router,  prefix="/products",  tags=["Products"])
 app.include_router(customers.router, prefix="/customers", tags=["Customers"])
 app.include_router(invoices.router,  prefix="/invoices",  tags=["Invoices"])
 app.include_router(stats.router,     prefix="/stats",     tags=["Stats"])
 
-
-@app.get("/")  
-def read_root():
-    return {"message": "Welcome to Quang Thuy Grocery Store API"}
+@app.get("/")
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
